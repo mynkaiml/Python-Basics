da387923166462ec9519620547fbfe875c99f014
Manvendra Singh chouhan <singhchouhanmanvendra7@gmail.com>
2025-12-29 23:40:00 +0530

    if - else - elif statements

--- 6.if-else-statements.py
+++ 6.if-else-statements.py
@@ -3,16 +3,23 @@
 #                   (if, elif, else)
 
 age = int(input("Enter your age here : "))
+has_ticket = True
+price = 10.0
 
+
+if age >= 65:
+    print("You are an senior")
+    print(f"The ticket price for a senior is ${price * 0.75}")
 
 if age >= 18:
-    print("You are eligible to give vote!")
-
-elif age < 0:
-    print("You havent been born yet!")
-
-elif age == 0:
-    print("You are a child!")
+    print("You are an adult")
+    print(f"The ticket price for an adult is ${price}")
 
 else:
-    print("You are not eligible!")
+    print("You are a child")
+    print(f"The ticket price for an child is ${price * 0.5}")
+
+if has_ticket:
+    print("you may enter, you have a ticket")
+else:
+    print("You need to buy a ticket")
