55c7f501f1f5c6f8249dd12e5cdefd6014d9b834
Manvendra Singh chouhan <singhchouhanmanvendra7@gmail.com>
2025-12-30 00:55:05 +0530

    This is a Python Basics Repo For Beginners

---
+++ BASICS/10_List.py
@@ -0,0 +1,21 @@
+# List [] = mutable, most flexible
+
+numbers = [10, 20, 30, 40, 50]
+print("Original list:", numbers)
+
+# Accessing elements
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+# Updating elements
+numbers[2] = 35
+print("Updated list:", numbers)
+
+# Adding elements
+numbers.append(60)
+print("After adding an element:", numbers)
+
+# Removing elements
+numbers.remove(20)
+print("After removing an element:", numbers)
+
